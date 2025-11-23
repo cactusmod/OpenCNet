88a11618b36b4a861b2b010b319beeefcb898874
Dwarslooper <dwarslooper2910@gmx.de>
2025-11-23 23:40:27 +0100

    impl commons in client, remove junit tests

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -2,18 +2,10 @@ plugins {
     id("java")
 }
 
-group = "com.dwarslooper.cactus.ocn"
-version = "1.0-SNAPSHOT"
-
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
+    implementation(project(":commons"))
 }
