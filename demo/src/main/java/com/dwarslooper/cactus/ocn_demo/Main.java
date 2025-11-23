f321a418f2737d0d0974181cfb685ad16cb86f72
Dwarslooper <dwarslooper2910@gmx.de>
2025-11-23 23:42:05 +0100

    update test cnet impl

--- demo/src/main/java/com/dwarslooper/cactus/ocn_demo/Main.java
+++ demo/src/main/java/com/dwarslooper/cactus/ocn_demo/Main.java
@@ -1,10 +1,14 @@
 package com.dwarslooper.cactus.ocn_demo;
 
+import com.dwarslooper.cactus.ocn_client.CNetClient;
+import com.dwarslooper.cactus.ocn_client.builder.CNetClientBuilder;
 import com.dwarslooper.cactus.ocn_commons.network.protocol.builder.ProtocolBuilder;
 import com.dwarslooper.cactus.ocn_commons.network.protocol.impl.ProtocolImpl;
 import com.dwarslooper.cactus.ocn_server.CNetServer;
 import com.dwarslooper.cactus.ocn_server.builder.CNetServerBuilder;
 import io.netty.channel.ChannelOption;
+
+import java.net.InetSocketAddress;
 
 public class Main {
 
@@ -23,6 +27,16 @@ public class Main {
                 .build();
 
         server.start();
+
+        CNetClient client = new CNetClientBuilder(new InetSocketAddress("127.0.0.1", 1414), protocol)
+                .timeout(20L)
+                .withChannelConfig(config -> {
+                    config.setOption(ChannelOption.SO_KEEPALIVE, true);
+                })
+                .useConnection(CustomClient::new)
+                .build();
+
+        client.connect();
     }
 
 }
