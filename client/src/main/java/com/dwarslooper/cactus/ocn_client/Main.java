dcd31e9056c02db091eb2a9db1e6a2f20ccdc100
Dwarslooper <dwarslooper2910@gmx.de>
2025-11-23 23:41:55 +0100

    remove autogenerated main classes

--- client/src/main/java/com/dwarslooper/cactus/ocn_client/Main.java
+++
@@ -1,7 +0,0 @@
-package com.dwarslooper.cactus.ocn;
-
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello, World!");
-	}
-}
--- commons/src/main/java/com/dwarslooper/cactus/ocn_commons/Main.java
+++
@@ -1,7 +0,0 @@
-package com.dwarslooper.cactus.ocn_commons;
-
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello, World!");
-	}
-}
